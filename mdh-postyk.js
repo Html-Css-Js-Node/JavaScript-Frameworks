590262751ed0095bdcec27e394b0db6161234ea7
Html-Css-Js-Node <96861016+Html-Css-Js-Node@users.noreply.github.com>
2022-01-23 22:06:02 +0200

    Update mdh-postyk.js

--- mdh-postyk.js
+++ mdh-postyk.js
@@ -1,4 +1,6 @@
-let a = prompt('Введите Цвет Фона:');
+alert ('Вы в программе:"Postyk". Здесь можно создавать посты прямо в браузере, делать скриншоты, и делиться ими в соц. сетях. с уважением, Берлина Андрея.');
+
+let a = prompt('Введите Цвет Фона на английском: blue, red, green, gray и т.п.');
 let body = document.getElementById("body");
 body.innerHTML = `
 <style id = "style">
@@ -11,18 +13,32 @@ body.innerHTML = `
 </footer>
 `;
 body.style.background = a;
+let header = document.getElementById('header');
+header.style.textAlign = 'center';
+header.style.color = 'rgb(255,91,91)';
+header.innerHTML=`
+<h1>Вы используете: Postyk</h1>
+<p>адрес программы:<a href = 'https://html-css-js-node.github.io/postyk/'>https://html-css-js-node.github.io/postyk/</а></p>
+`;
 
-let b = prompt('Введите Цвет Шрифта:');
-let c = prompt('Введите Размер Шрифта:');
-let d = prompt('Введите Позицию Текста:');
+
+let b = prompt('Введите Цвет Шрифта на английском: red, blue, green, gray и т.п.');
+let c = prompt('Введите Размер Шрифта: от 0px до 1000px');
+let d = prompt('Введите Позицию Текста: center, left, right');
 let section = document.getElementById('section');
 section.style.color = b;
 section.style.fontSize = c;
 section.style.textAlign = d;
 section.innerHTML = `
-<p id="p"></p>
+<p id="p1"></p>
 
 `;
 let h = prompt('Введите Текст:')
-let p = document.getElementById('p');
-p.innerHTML = h;
+let p1 = document.getElementById('p1');
+p1.innerHTML = h;
+
+let footer = document.getElementById('footer');
+footer.innerHTML=`
+<p id="p2">С уважением, Берлин Андрей.</p>`;
+footer.style.textAlign = 'center';
+footer.style.color= 'rgb(255,91,91)';
