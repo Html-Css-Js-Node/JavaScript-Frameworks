bee3365a2757bb8a091e0c4b35c953b8c6ebeb40
Андрей Берлин <96861016+WebAppAndroid@users.noreply.github.com>
2022-10-10 10:32:25 +0300

    Update mdh-music-system.js

--- mdh-music-system.js
+++ mdh-music-system.js
@@ -34,17 +34,17 @@ header.innerHTML = `
 `;
 let section = document.getElementById("section");
 section.innerHTML = `
-        <iframe id="inlineFrameExample" title="Inline Frame Example" width="300" height="500" src="https://html-css-js-node.github.io/ai-piano-1/">
+        <iframe id="inlineFrameExample" title="Inline Frame Example" width="300" height="500" src="https://webappandroid.github.io/ai-piano-1/">
         </iframe>
 
 
         <p>Это интерактивное Пианино-1 с Исскуственным Интеллектом (ИИ). Если вы нажмете на клавишу пианино, то ИИ создаст дополнительную мелодию.<a href="https://html-css-js-node.github.io/ai-piano-1/" target="_blank"> Открыть на полный экран в новом окне.</a></p>
 
-        <br><br><iframe id="inlineFrameExample" title="Inline Frame Example" width="300" height="500" src="https://html-css-js-node.github.io/ai-piano-2/">
+        <br><br><iframe id="inlineFrameExample" title="Inline Frame Example" width="300" height="500" src="https://webappandroid.github.io/ai-piano-2/">
     </iframe>
 
         <p>Это интерактивное Пианино-2 с Исскуственным Интеллектом (ИИ). Если вы нажмете на клавишу пианино, то ИИ создаст дополнительную мелодию.<a href="https://html-css-js-node.github.io/ai-piano-2/" target="_blank"> Открыть на полный экран в новом окне.</a></p>
-        <br><br><iframe id="inlineFrameExample" title="Inline Frame Example" width="300" height="500" src="https://html-css-js-node.github.io/Ai-Neural-Drum-Machine/">
+        <br><br><iframe id="inlineFrameExample" title="Inline Frame Example" width="300" height="500" src="https://webappandroid.github.io/Ai-Neural-Drum-Machine/">
     </iframe>
 
         <p>Это интерактивный Барабан с Исскуственным Интеллектом (ИИ). Если вы нажмете на клавишу обновить, то ИИ создаст дополнительную мелодию.<a href="https://html-css-js-node.github.io/Ai-Neural-Drum-Machine/" target="_blank"> Открыть на полный экран в новом окне.</a></p>
